5580b2ca9ab81a31bee9bcda21571c3308e8cb30
isaac-svg <isaacsakyi009@gmail.com>
2023-12-09 21:37:43 +0000

    phase1

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,22 +6,21 @@ Unittest classes:
     TestFileStorage_methods
 """
 import os
-import json
 import models
 import unittest
 from datetime import datetime
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
 from models.user import User
 from models.state import State
 from models.place import Place
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class TestFileStorage_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the FileStorage class."""
+    """Unittests for instantiation of the FileStorage class."""
 
     def test_FileStorage_instantiation_no_args(self):
         self.assertEqual(type(FileStorage()), FileStorage)
@@ -41,7 +40,7 @@ class TestFileStorage_instantiation(unittest.TestCase):
 
 
 class TestFileStorage_methods(unittest.TestCase):
-    """Unittests for testing methods of the FileStorage class."""
+    """Unittests for methods of the FileStorage class."""
 
     @classmethod
     def setUp(self):
